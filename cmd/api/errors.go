d251daa853c0c95790d4a5f7ca88c5c29d5f5911
Pedro Moreira <pedro.moreira@emma-sleep.com>
2025-04-07 18:32:24 +0000

    Finish Validating JSON Input Chapter

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -27,9 +27,6 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
 	env := envelope{"error": message}
 
-	// Write the response using the writeJSON() helper. If this happens to return an
-	// error then log it, and fall back to sending the client an empty response with a
-	// 500 Internal Server Error status code.
 	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
 		app.logError(r, err)
@@ -60,4 +57,10 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+// Note that the errors parameter here has the type map[string]string, which is exactly
+// the same as the errors map contained in our Validator type.
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
