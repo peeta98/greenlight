625ccb9391a89956a67ec8fdba128334243d1498
Pedro Moreira <pedro.moreira@emma-sleep.com>
2025-04-18 13:34:13 +0000

    Finish Global Rate Limiting Chapter

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,13 +8,7 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
-	// Convert the notFoundResponse() helper to a http.Handler using the
-	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
-	// Not found responses.
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-
-	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler and set
-	// it as the custom error handler for 405 Method Not Allowed responses.
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
@@ -25,6 +19,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
-	// Wrap the router with the panic recovery middleware.
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
