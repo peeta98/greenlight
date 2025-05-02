80fcda240cf89b24ee6767fb99de3f9b8b202338
Pedro Moreira <pedro.moreira@emma-sleep.com>
2025-05-02 14:42:04 +0000

    Finish Simple CORS Requests Chapter

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -204,3 +204,23 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 
 	return app.requireActivatedUser(fn)
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Warn the client that the Origin Header may vary from client to client
+		w.Header().Add("Vary", "Origin")
+
+		origin := r.Header.Get("Origin")
+
+		if origin != "" {
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					break
+				}
+			}
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
